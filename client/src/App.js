a3b4c9116cc8481f255ec8dc12e295aaa4732038
WebbkodsLarlingen <webbkodslarlingen@gmail.com>
2023-12-05 03:12:44 +0100

    TailwindCSS+ReactJS live funkar

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -10,11 +10,10 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="text-xs"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
