6f0cba131a584dc65a14a3698f444548b87aa534
WebbkodsLarlingen <webbkodslarlingen@gmail.com>
2024-01-09 06:27:50 +0100

    Påbörjat Inloggning + React Router

---
+++ client/src/router/Login.js
@@ -0,0 +1,20 @@
+import "../App.css";
+import { useState, useEffect } from "react";
+
+function Login() {
+  const [state1, setState1] = useState(null);
+  const [state2, setState2] = useState(null);
+  const [state3, setState3] = useState(null);
+
+  useEffect(() => {
+    // Init
+
+    // Cleanup
+    return () => {};
+  }, []);
+
+  // Returned JSX DOM Data Dynamic
+  return <></>;
+}
+
+export default Login; // First letter always uppercase!
